db338b00c9d8e402ac2418ece42e211831a6cbeb
Sriram <gh.sriram@solera.com>
2024-08-09 15:37:52 +0530

    Add initial .NET MAUI project with multi-platform support

---
+++ src/DueAlert/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DueAlert
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
